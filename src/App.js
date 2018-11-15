08bb9d2cb9edb33369987363280543384ab0a2e7
Oleh <zhyriki@ukr.net>
2018-11-15 23:50:09 +0200

    first component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import Car from './Car/Car'
 
 class App extends Component {
+
+  state = {
+    cars: [
+      {name: 'ZAZ', year: 2000},
+      {name: 'Audi', year: 2018},
+      {name: 'Audi', year: 2012}
+    ]
+  }
+
   render() {
+    const cars = this.state.cars
     return (
       <div className="App">
         <h1 style={{color: 'red', textTransform: 'uppercase'}}>Hello world!</h1>
+        <Car name={cars[0].name} year={cars[0].year} />
+        <Car name={cars[1].name} year={cars[1].year} />
+        <Car name={cars[2].name} year={cars[2].year} />
       </div>
     );
   }
---
+++ src/Car/Car.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default (props) => (
+  <div style={{fontWeight: 600, fontSize: '20px', border: '1px solid black', margin: '10px'}}>
+    <h1>{ props.name }</h1>
+    <p>Year: { props.year } </p>
+  </div>
+)
