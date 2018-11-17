61fbc96482fbab2ac784504a59f74ec4ca707dd9
Oleh <zhyriki@ukr.net>
2018-11-17 16:18:44 +0200

    style

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import './Car.css'
 
-export default (props) => (
-  <div style={{fontWeight: 600, fontSize: '20px', border: '1px solid black', margin: '10px'}}>
-    <h1>{ props.name }</h1>
-    <p>Year: { props.year } </p>
-    <input onChange={props.writeNewName}/>
-    <button onClick={props.delete}>Delete</button>
-  </div>
-)
+export default (props) => {
+  const inputStyle = ['input']
+  if (props.name !== '') inputStyle.push('input-green')
+  else inputStyle.push('input-red')
+  if (props.name.length > 4) inputStyle.push('input-valid')
+  return (
+    <div className="Car">
+      <h1>{ props.name }</h1>
+      <p>Year: { props.year } </p>
+      <input onChange={props.writeNewName} className={inputStyle.join(' ')} value={props.name} />
+      <button onClick={props.delete}>Delete</button>
+    </div>
+  )
+}
